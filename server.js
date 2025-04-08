7b7ee251df1b50baddb0e015b022d0990b1a673b
M6F\student <lyw1212@gmail.com>
2025-04-08 16:52:03 +0900

    server 제발

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-// server.js
-import { create, router as _router, defaults } from 'json-server';
-const server = create();
-const router = _router('.data/db.json');
-const middlewares = defaults();
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('.data/db.json');
+const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(3000, () => {
-  console.log('JSON Server is running');
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
 });
