89bbc96846b90533fe95216094834a150982724f
wony0321 <lyw1212@gmail.com>
2025-04-08 20:27:24 +0900

    server update

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('.data/db.json');
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
+server.listen(3000, () => {
+  console.log('JSON Server is running');
 });
